56716e045098acebd1cdb78855b320953bfb91da
Oleg V. Kuzmin <inpassor@yandex.com>
2018-10-26 22:22:16 +0300

    config.aws.appSync.validateBody parameter has been added

--- lib/interfaces.d.ts
+++ lib/interfaces.d.ts
@@ -122,6 +122,7 @@ export interface AppConfig {
             accessKeyId: string;
             region: string;
             authHeaderName?: string;
+            validateBody?: boolean;
         };
     };
 }
--- src/helpers/validate-aws4.ts
+++ src/helpers/validate-aws4.ts
@@ -1,5 +1,5 @@
-import {ExpressRequest} from '../interfaces';
 import {AWS4} from '@inpassor/functions';
+import {ExpressRequest} from '../interfaces';
 
 export const validateAWS4 = (request: ExpressRequest): boolean => {
     const config = request.app.config;
@@ -10,7 +10,9 @@ export const validateAWS4 = (request: ExpressRequest): boolean => {
         return AWS4.validateAuthorizationHeader(
             authorizationHeader,
             AWS4.getAmzDate(request.headers),
-            request.rawBody.toString(),
+            (config.aws.appSync.validateBody === undefined || config.aws.appSync.validateBody) ?
+                request.rawBody.toString()
+                : '',
             config.aws.appSync.accessKeyId,
             config.aws.appSync.region
         );
