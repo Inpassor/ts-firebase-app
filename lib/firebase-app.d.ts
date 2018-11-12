a2b3661dd21b40a5a2c4a4321fdd2af85911b81c
Oleg Kuzmin <oleg.kuzmin@arcadia.spb.ru>
2018-11-12 17:44:44 +0300

    async config get support

--- lib/firebase-app.d.ts
+++ lib/firebase-app.d.ts
@@ -1,3 +1,3 @@
 import * as functions from 'firebase-functions';
 import { AppConfig } from './interfaces';
-export declare const firebaseApp: (config: AppConfig) => functions.HttpsFunction;
+export declare const firebaseApp: (getConfig: AppConfig | Promise<AppConfig>) => functions.HttpsFunction;
--- src/firebase-app.ts
+++ src/firebase-app.ts
@@ -3,6 +3,12 @@ import * as functions from 'firebase-functions';
 import {expressApp} from './express-app';
 import {AppConfig} from './interfaces';
 
-export const firebaseApp = (config: AppConfig) => {
-    return functions.https.onRequest(expressApp(config));
+export const firebaseApp = (getConfig: AppConfig | Promise<AppConfig>): functions.HttpsFunction => {
+    return functions.https.onRequest(() => {
+        Promise.resolve(getConfig).then((config: AppConfig): void => {
+            expressApp(config);
+        }, (error: any) => {
+            console.error(error);
+        });
+    });
 };
