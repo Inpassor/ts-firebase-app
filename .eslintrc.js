2569402eccb44c6cb19dd177e2f7f59c39f06b0e
Oleg Kuzmin <oleg.kuzmin@arcadia.spb.ru>
2020-01-27 10:33:21 +0300

    0.3.39

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    overrides: [
+        {
+            files: ['*.ts'],
+            parser: '@typescript-eslint/parser',
+            extends: [
+                'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+                // 'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+                // 'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+            ],
+            parserOptions: {
+                ecmaVersion: 2018,
+                project: 'tsconfig.json',
+                sourceType: 'module',  // Allows for the use of imports
+            },
+            rules: {
+                // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+                // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
+                '@typescript-eslint/no-empty-function': 'off',
+            },
+        }
+    ]
+};
